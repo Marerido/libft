db83a1386d39a2b3b65c92b104df1a4852880293
Marerido <144279268+Marerido@users.noreply.github.com>
2024-04-07 22:57:55 +0200

    Add files via upload
    add ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int stringlen;
+	unsigned int sizesub;
+	unsigned int end;
+	char *substr;
+
+	stringlen = ft_strlen((char *)s);
+	end = 0;
+	if (start < stringlen)
+		end = len;
+	if (end = len)
+		end = len;
+	if (s == NULL)
+		return (NULL);
+	substr = (char *)malloc(sizeof(char) * (sizesub + 1));
+	if (!substr)
+		return (NULL);
+	ft_memmove(substr, s + start, len);
+	substr[len] = '\0';
+	return (substr);
+}
